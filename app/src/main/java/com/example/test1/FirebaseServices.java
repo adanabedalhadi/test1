8da63a3fa80204dccc135ff60418e74b73c668da
adan <abdelhadeadan@gmail.com>
2022-11-10 14:16:15 +0200

    finsh

---
+++ app/src/main/java/com/example/test1/FirebaseServices.java
@@ -0,0 +1,16 @@
+package com.example.test1;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class FirebaseServices {
+    private static FirebaseServices instance;
+    private FirebaseAuth auth;
+
+    public static FirebaseServices getInstance() {
+        return instance;
+    }
+
+    public FirebaseAuth getAuth() {
+        return auth;
+    }
+}
---
+++ app/src/main/java/com/example/test1/LoginFragment.java
@@ -0,0 +1,111 @@
+package com.example.test1;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LoginFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LoginFragment extends Fragment {
+
+    private EditText etPassword,etUsername;
+    private Button btnLogin;
+    private FirebaseAuth mAuth;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LoginFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LoginFragment newInstance(String param1, String param2) {
+        LoginFragment fragment = new LoginFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        connectComponents();
+
+    }
+
+    private void connectComponents() {
+        etUsername= getView().findViewById(R.id.etUsernameLogin);
+        etPassword= getView().findViewById(R.id.etPasswordLogin);
+        btnLogin= getView().findViewById(R.id.btnLogin);
+        mAuth= FirebaseAuth.getInstance();
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username=etUsername.getText().toString();
+                String password=etPassword.getText().toString();
+               mAuth.signInWithEmailAndPassword(username, password)
+                        .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    //
+                                } else {
+                                    // PUT YOUR CODE HERE
+                                }
+                            }
+                        });
+
+
+            }
+        });
+    }
+}
